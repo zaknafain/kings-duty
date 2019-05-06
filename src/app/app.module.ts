1c0d39b29e9f6c0d451943e986922e13e623c805
David Seeherr <david.seeherr@gmail.com>
2019-05-06 21:41:50 +0200

    Add the map to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,26 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 import { AppComponent } from './app.component';
+import { MapComponent } from './map/map.component';
 import { TileComponent } from './map/tile/tile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    MapComponent,
     TileComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/map/map.component.ts
@@ -0,0 +1,88 @@
+
+import { Component, HostBinding } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { TileService } from './tile/tile.service';
+import { Tile } from './tile/tile';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: [
+    './map.component.scss',
+    '../floating-elements.scss'
+  ]
+})
+export class MapComponent {
+  rowLength: number;
+  hexWidth = 100;
+  filters = [
+    { name: 'terrain', icon: 'terrain' },
+    { name: 'coordinates', icon: 'location_on' },
+    { name: 'region', icon: 'map' }
+  ];
+  hoverFilter = 'terrain';
+  chosenTile: Tile;
+  @HostBinding('attr.style')
+  public get valueAsStyle(): any {
+    return this.sanitizer.bypassSecurityTrustStyle(`--hex-width: ${this.hexWidth}px`);
+  }
+
+  constructor(
+    private sanitizer: DomSanitizer,
+    private tileService: TileService
+  ) {
+    this.tileService.tiles$.subscribe(tiles => {
+      this.rowLength = tiles.filter(tile => tile.y === 0).length;
+    });
+  }
+
+  clickTile(tile: Tile): void {
+    if (!tile.isKnown) {
+      this.tileService.revealTile(tile);
+    }
+    if (this.chosenTile === tile) {
+      this.chosenTile = undefined;
+    } else {
+      this.chosenTile = tile;
+    }
+  }
+
+  calculateGridColumns(): object {
+    let columns = new Array<string>(this.rowLength);
+    columns = columns.fill(`${this.hexWidth}px`);
+
+    return {
+      'grid-template-columns': columns.join(' ')
+    };
+  }
+
+  calculateTileClasses(tile: Tile): string {
+    let classes = 'hexagon';
+    if (tile.y % 2 !== 0) { classes = `${classes} offset`; }
+    if (tile.isKnown) { classes = `${classes} ${tile.terrain}`; }
+    if (this.tileEquals(this.chosenTile, tile)) { classes = `${classes} focused`; }
+
+    return classes;
+  }
+
+  reduceHexagonSize(): void {
+    if (this.hexWidth > 0) {
+      this.hexWidth = this.hexWidth - 8;
+    }
+  }
+
+  raiseHexagonSize(): void {
+    if (this.hexWidth < 100) {
+      this.hexWidth = this.hexWidth + 8;
+    }
+  }
+
+  updateHoverFilter(filter: string) {
+    this.hoverFilter = filter;
+  }
+
+  private tileEquals(a: Tile, b: Tile): boolean {
+    return a && b && a.x === b.x && a.y === b.y ? true : false;
+  }
+
+}
