ecfc8c1c1e60f47f9430e50bdac580517dbfcf54
David Seeherr <david.seeherr@gmail.com>
2019-05-27 15:46:57 +0200

    Add terrain specs

--- src/app/map/tiles/terrain.spec.ts
+++ src/app/map/tiles/terrain.spec.ts
@@ -1,7 +1,27 @@
-import { Terrain } from './terrain';
+import { Terrain, terrainTypes } from './terrain';
 
 describe('Terrain', () => {
   it('should create an instance', () => {
     expect(new Terrain()).toBeTruthy();
+  });
+});
+
+describe('terrainTypes', () => {
+  it('should be an array of Terrain', () => {
+    terrainTypes.forEach(terrain => {
+      expect(terrain.label).toBeDefined();
+      expect(terrain.regionSizeMax).toBeDefined();
+      expect(terrain.regionSizeMin).toBeDefined();
+      expect(terrain.weight).toBeDefined();
+    });
+  });
+
+  it('should be an array of unique Terrains', () => {
+    let label: string;
+
+    terrainTypes.forEach(terrain => {
+      label = terrain.label;
+      expect(terrainTypes.filter(tt => tt.label === label).length).toBe(1);
+    });
   });
 });
