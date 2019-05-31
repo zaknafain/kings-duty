09e74905908b0d02eb0a5024805a57a201cf9ec2
David Seeherr <david.seeherr@gmail.com>
2019-05-31 14:05:43 +0200

    Add realm service specs

--- src/app/realms/realm.service.spec.ts
+++ src/app/realms/realm.service.spec.ts
@@ -1,12 +1,39 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, fakeAsync } from '@angular/core/testing';
 
 import { RealmService } from './realm.service';
+import { Realm } from './realm';
 
 describe('RealmService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: RealmService;
+  const defaultRealm: Realm = {
+    ruler: 'Ruler',
+    name: 'Name',
+    size: 1,
+    people: 1234
+  };
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({ });
+
+    service = new RealmService();
+  });
 
   it('should be created', () => {
-    const service: RealmService = TestBed.get(RealmService);
     expect(service).toBeTruthy();
   });
+
+  it('should initially return a realm', () => {
+    expect(service.playerRealm).toBeDefined();
+    expect(service.playerRealm.name).toBeDefined();
+    expect(service.playerRealm.ruler).toBeDefined();
+    expect(service.playerRealm.size).toBe(0);
+  });
+
+  it('playerRealm$ should be an Observable of playerRealm', fakeAsync(() => {
+    let playerRealm = {};
+    service.playerRealm$.subscribe(r => { playerRealm = r; });
+    service.playerRealm = defaultRealm;
+
+    expect(playerRealm).toEqual(defaultRealm);
+  }));
 });
