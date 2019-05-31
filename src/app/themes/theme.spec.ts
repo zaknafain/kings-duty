c93c5b3e94f52bd0d539c04fe5c293107736f1b9
David Seeherr <david.seeherr@gmail.com>
2019-05-31 14:27:41 +0200

    Add theme spec

--- src/app/themes/theme.spec.ts
+++ src/app/themes/theme.spec.ts
@@ -1,7 +1,15 @@
-import { Theme } from './theme';
+import { Theme, colorThemes, defaultTheme } from './theme';
 
 describe('Theme', () => {
   it('should create an instance', () => {
     expect(new Theme()).toBeTruthy();
+  });
+
+  it('colorThemes should return an array of themes', () => {
+    expect(colorThemes).toBeDefined();
+  });
+
+  it('defaultTheme should return a theme', () => {
+    expect(defaultTheme).toBeDefined();
   });
 });
