1b5791bf60a033758be12978a4a374ccd5a0b481
David Seeherr <david.seeherr@gmail.com>
2019-08-29 11:29:37 +0200

    update karma to use spec reporter

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -10,7 +10,8 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-spec-reporter'),
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
@@ -20,7 +21,7 @@ module.exports = function (config) {
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['spec'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
@@ -33,6 +34,7 @@ module.exports = function (config) {
           '--headless',
           '--disable-gpu',
           '--no-sandbox',
+          '--disable-web-security',
           '--remote-debugging-port=9222',
           '--disable-translate',
           '--disable-extensions'
